d3929628b32e90225fa157914812ca19cb1f690e
Kevin Tracy <kevt.laser@gmail.com>
2022-04-18 12:45:46 -0400

    changed initialization and cleaned up STC

--- examples/rocket_landing_ineq_calipso_stc.jl
+++ examples/rocket_landing_ineq_calipso_stc.jl
@@ -47,7 +47,6 @@ dt = Dynamics(
 dyn = [dt for t = 1:T-1]
 
 # ## initialization
-# x1 = [3.0; 2.0; 1.0; 0.0; 0.0; 0.0]
 x1 = [-5.0;0;5;0;0;0]
 xT = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0]
 
@@ -68,46 +67,36 @@ Fy_max = 10.0#8.0
 Fz_min = 0.0#6.0
 Fz_max = 20.0#12.5
 
-function eq_stc(x,u,w)
-    gm = u[4]
-    gp = u[5]
-    cm = u[6]
-    cp = u[7]
-    # a = -3.0
-    # b = 1.0
 
-    [gp - gm - (-x[1] + a);
-     cp - cm - (x[3] - b);
-     gp*cm]
-end
+# state triggered constraints
+# if g(x) > 0, then c(x) ≧ 0
+# g(x) = -x[1] + a > 0
+# c(x) = x[3] - b ≧ 0
+#
+# this is handled by introducing 4 new variables at each time step as controls
+# g+,g-,c+,c- all ≥ 0
+# g+ - g- = g(x)
+# c+ - c- = c(x)
+# g+ ⋅ c- = 0
+
 a = -1.0
 b = 3.0
+
 eq1 = Constraint((x, u, w) -> x - x1, num_state, num_action)
 eqT = Constraint((x, u, w) -> x - xT, num_state, 0)
-eqSTC = Constraint((x,u,w) -> [u[5] - u[4] - (-x[1] + a);
-                               u[7] - u[6] - (x[3] - b);
-                               u[5]*u[6]],num_state,num_action)
-# eq = [eq1, [Constraint() for t = 2:T-1]..., eqT]
+eqSTC = Constraint((x,u,w) -> [u[5] - u[4] - (-x[1] + a);  # g+ - g- = -x[1] + a
+                               u[7] - u[6] - (x[3] - b);   # c+ - c- = x[3] - b
+                               u[5]*u[6]],                 # g+ ⋅ c- = 0
+                               num_state,num_action)
 eq = [eq1, [eqSTC for t = 2:T-1]...,eqT]
 
-# function stc_g(x)
-#     a = -3.0
-#     x[1] - a
-# end
-# function stc_c(x)
-#     b = 1.0
-#     -x[3] + b
-# end
 
 ineq = [[Constraint((x, u, w) ->
     [
         u[1] - Fx_min; Fx_max - u[1];
         u[2] - Fy_min; Fy_max - u[2];
         u[3] - Fz_min; Fz_max - u[3];
-        u[4:7]
-        # -u[4]; # -σ ≥ 0
-        # -u[4] + stc_g(x);
-         # u[4]*stc_c(x)
+        u[4:7]                         # g+,g-,c+,c- all ≥ 0
     ], num_state, num_action
 ) for t = 1:T-1]..., Constraint()]
 
@@ -117,7 +106,8 @@ so = [[Constraint()] for t = 1:T]
 trajopt = CALIPSO.TrajectoryOptimizationProblem(dyn, obj, eq, ineq, so)
 
 # ## initialize
-# x_interpolation = linear_interpolation(x1, xT, T)
+using Random
+Random.seed!(2)
 x_interpolation = [x1 + 0.1*randn(6) for t = 1:T ]
 u_guess = [1.0 * randn(num_action) for t = 1:T-1]
 
@@ -141,18 +131,7 @@ x_sol, u_sol = get_trajectory(solver, trajopt)
 
 Xm = hcat(x_sol...)
 Um = hcat(u_sol...)
-# mat"
-# figure
-# hold on
-# plot3($Xm(1,:),$Xm(2,:),$Xm(3,:))
-# for i = 1:$T-1
-#     s = 10;
-#     quiver3($Xm(1,i),$Xm(2,i),$Xm(3,i),$Um(1,i)/s,$Um(2,i)/s,$Um(3,i)/s,'off')
-# end
-# axis equal
-# view(34,10)
-# hold off
-# "
+
 using MATLAB
 mat"
 figure
@@ -172,27 +151,3 @@ hold on
 plot($Um')
 hold off
 "
-
-# for i = 1:T-1
-#     u = u_sol[i]
-#     x = x_sol[i]
-#     if ((stc_g(x)<0) & (stc_c(x)<=0)) | (stc_g(x)>=0)
-#
-#     else
-#         @show x
-#         error("this violates")
-#     end
-# end
-# # ## state
-# using Plots
-# plot(hcat(x_sol...)',
-#     label=["px" "py" "pz" "vx" "vy" "vz"])
-#
-# # ## control
-# plot(hcat(u_sol[1:end-1]..., u_sol[end-1])',
-#     linetype=:steppost,
-#     legend=:topleft,
-#     label=["Fx" "Fy" "Fz"])
-#
-# soc_check = [norm(u[1:2]) < u[3] for u in u_sol]
-# all(soc_check)
