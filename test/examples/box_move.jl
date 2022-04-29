6b6254d4167a082d5a66f48085f52ce6be5ed719
taylor <thowell@stanford.edu>
2022-04-28 23:18:07 -0700

    trajopt problem clean up

--- test/examples/box_move.jl
+++ test/examples/box_move.jl
@@ -1,5 +1,5 @@
 @testset "Examples: Box move (linear cone)" begin
-    function box_dyn(mass_matrix, dynamics_bias, h, y, x, u, w)
+    function box_dyn(mass_matrix, dynamics_bias, timestep, y, x, u)
         model = RoboDojo.box
         
         # configurations
@@ -7,7 +7,7 @@
         q2⁻ = x[3 .+ (1:3)]
         q2⁺ = y[1:3]
         q3⁺ = y[3 .+ (1:3)]
-    
+
         # control
         u_control = u[1:3]
         γ = u[3 .+ (1:4)]
@@ -21,24 +21,27 @@
         [
         q2⁺ - q2⁻;
         RoboDojo.dynamics(model, mass_matrix, dynamics_bias,
-            h, q1⁻, q2⁺, u_control, zeros(model.nw), λ, q3⁺)
+            timestep, q1⁻, q2⁺, u_control, zeros(model.nw), λ, q3⁺)
         ]
     end
-    function box_dyn1(mass_matrix, dynamics_bias, h, y, x, u, w)
+
+    function box_dyn1(mass_matrix, dynamics_bias, timestep, y, x, u)
         [
-        box_dyn(mass_matrix, dynamics_bias, h, y, x, u, w);
+        box_dyn(mass_matrix, dynamics_bias, timestep, y, x, u);
         y[6 .+ (1:4)] - u[3 .+ (1:4)];
         y[6 + 4 .+ (1:6)] - x;
         ]
     end
-    function box_dynt(mass_matrix, dynamics_bias, h, y, x, u, w)
+
+    function box_dynt(mass_matrix, dynamics_bias, timestep, y, x, u)
         [
-        box_dyn(mass_matrix, dynamics_bias, h, y, x, u, w);
+        box_dyn(mass_matrix, dynamics_bias, timestep, y, x, u);
         y[6 .+ (1:4)] - u[3 .+ (1:4)];
         y[6 + 4 .+ (1:6)] - x[6 + 4 .+ (1:6)];
         ]
     end
-    function contact_constraints_inequality_1(h, x, u, w)
+
+    function contact_constraints_inequality_1(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -52,7 +55,8 @@
         fc;
         ]
     end
-    function contact_constraints_inequality_t(h, x, u, w)
+
+    function contact_constraints_inequality_t(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -66,8 +70,8 @@
         fc;
         ]
     end
-    
-    function contact_constraints_inequality_T(h, x, u, w)
+
+    function contact_constraints_inequality_T(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -76,8 +80,8 @@
         ϕ;
         ]
     end
-    
-    function contact_constraints_equality_1(h, x, u, w)
+
+    function contact_constraints_equality_1(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -87,7 +91,7 @@
         η = u[3 + 4 + 8 + 4 .+ (1:8)]
         μ = model.friction_body_world
         fc = μ .* γ[1:4] - [sum(β[1:2]); sum(β[3:4]); sum(β[5:6]); sum(β[7:8])]
-        v = (q3 - q2) ./ h[1]
+        v = (q3 - q2) ./ timestep[1]
         E = [1.0; -1.0]
         vT = vcat([E * (RoboDojo.box_contact_kinematics_jacobians[i](q3) * v)[1] for i = 1:4]...)
         ψ_stack = vcat([ψ[i] * ones(2) for i = 1:4]...)
@@ -97,8 +101,8 @@
         ψ .* fc;
         ]
     end
-    
-    function contact_constraints_equality_t(h, x, u, w)
+
+    function contact_constraints_equality_t(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -110,7 +114,7 @@
         γ⁻ = x[nx .+ (1:4)]
         μ = model.friction_body_world
         fc = μ .* γ[1:4] - [sum(β[1:2]); sum(β[3:4]); sum(β[5:6]); sum(β[7:8])]
-        v = (q3 - q2) ./ h[1]
+        v = (q3 - q2) ./ timestep[1]
         E = [1.0; -1.0]
         vT = vcat([E * (RoboDojo.box_contact_kinematics_jacobians[i](q3) * v)[1] for i = 1:4]...)
         ψ_stack = vcat([ψ[i] * ones(2) for i = 1:4]...)
@@ -121,7 +125,8 @@
         ψ .* fc; 
         ]
     end
-    function contact_constraints_equality_T(h, x, u, w)
+
+    function contact_constraints_equality_T(timestep, x, u)
         model = RoboDojo.box
         q2 = x[1:3]
         q3 = x[3 .+ (1:3)]
@@ -131,132 +136,139 @@
         γ⁻ .* ϕ;
         ]
     end
-    
+
     # ## horizon
-    T = 11
-    h = 0.1
-    
-    # ## box
+    horizon = 11
+    timestep = 0.1
+
+    # ## dimensions
     nx = 2 * RoboDojo.box.nq
     nu = RoboDojo.box.nu + 4 + 8 + 4 + 8
-    
+
+    num_states = [nx, [2 * nx + 4 for t = 2:horizon]...] 
+    num_actions = [nu for t = 1:horizon-1] 
+
     ## model
     mass_matrix, dynamics_bias = RoboDojo.codegen_dynamics(RoboDojo.box)
-    d1 = CALIPSO.Dynamics((y, x, u, w) -> box_dyn1(mass_matrix, dynamics_bias, [h], y, x, u, w), 2 * nx + 4, nx, nu)
-    mass_matrix, dynamics_bias = RoboDojo.codegen_dynamics(RoboDojo.box)
-    dt = CALIPSO.Dynamics((y, x, u, w) -> box_dynt(mass_matrix, dynamics_bias, [h], y, x, u, w), 2 * nx + 4, 2 * nx + 4, nu)
-    
-    dyn = [d1, [dt for t = 2:T-1]...];
-    
+
+    dynamics = [
+            (y, x, u) -> box_dyn1(mass_matrix, dynamics_bias, [timestep], y, x, u), 
+            [(y, x, u) -> box_dynt(mass_matrix, dynamics_bias, [timestep], y, x, u) for t = 2:horizon-1]...,
+    ];
+
     # ## initial conditions
     q1 = [0.0; 0.5; 0.0]
     qM = [0.0; 0.5; 0.0]
     # qT = [0.0; 0.5 + 1.0; 0.0]
     qT = [1.0; 0.5; 0.0]
-    
+
     q_ref = qT
-    x1 = [q1; q1]
+    state_initial = [q1; q1]
     xM = [qM; qM]
-    xT = [qT; qT]
+    state_goal = [qT; qT]
     x_ref = [q_ref; q_ref]
-    
+
     # ## objective
-    function obj1(x, u, w)
+    function obj1(x, u)
         J = 0.0
         J += 0.5 * transpose(x - x_ref) * Diagonal([1.0; 1.0; 1.0; 1.0; 1.0; 1.0]) * (x - x_ref)
         J += 0.5 * transpose(u) * Diagonal([1.0e-2 * ones(RoboDojo.box.nu); 1.0e-5 * ones(nu - RoboDojo.box.nu)]) * u
         return J
     end
-    function objt(x, u, w)
+
+    function objt(x, u)
         J = 0.0
         J += 0.5 * transpose(x[1:nx] - x_ref) * Diagonal([1.0; 1.0; 1.0; 1.0; 1.0; 1.0]) * (x[1:nx] - x_ref)
         J += 0.5 * transpose(u) * Diagonal([1.0e-2 * ones(RoboDojo.box.nu); 1.0e-5 * ones(nu - RoboDojo.box.nu)]) * u
         return J
     end
-    function objT(x, u, w)
+
+    function objT(x, u)
         J = 0.0
         J += 0.5 * transpose(x[1:nx] - x_ref) * Diagonal([1.0; 1.0; 1.0; 1.0; 1.0; 1.0]) * (x[1:nx] - x_ref)
         return J
     end
-    c1 = CALIPSO.Cost(obj1, nx, nu)
-    ct = CALIPSO.Cost(objt, 2 * nx + 4, nu)
-    cT = CALIPSO.Cost(objT, 2 * nx + 4, 0)
-    obj = [c1, [ct for t = 2:T-1]..., cT];
-    
+
+    objective = [
+            obj1, 
+            [objt for t = 2:horizon-1]..., 
+            objT,
+    ];
+
     # ## constraints
-    function equality_1(x, u, w)
+    function equality_1(x, u)
         [
-        contact_constraints_equality_1(h, x, u, w);
-        x - x1;
+            contact_constraints_equality_1(timestep, x, u);
+            x - state_initial;
         ]
     end
-    function equality_t(x, u, w)
+    function equality_t(x, u)
         [
-        contact_constraints_equality_t(h, x, u, w);
+            contact_constraints_equality_t(timestep, x, u);
         ]
     end
-    function equality_T(x, u, w)
+    function equality_T(x, u)
         [
-        contact_constraints_equality_T(h, x, u, w);
-        x[1:6] - xT;
+            contact_constraints_equality_T(timestep, x, u);
+            x[1:6] - state_goal;
         ]
     end
-    eq1 = CALIPSO.Constraint(equality_1, nx, nu)
-    eqt = CALIPSO.Constraint(equality_t, 2nx + 4, nu)
-    eqT = CALIPSO.Constraint(equality_T, 2nx + 4, 0)
-    eq = [eq1, [eqt for t = 2:T-1]..., eqT];
-    function inequality_1(x, u, w)
+
+    equality = [
+            equality_1, 
+            [equality_t for t = 2:horizon-1]..., 
+            equality_T,
+    ];
+
+    function inequality_1(x, u)
         [
-        contact_constraints_inequality_1(h, x, u, w);
-        u - [-10.0; -10.0; zeros(nu - 2)];
-        [10.0; 10.0] - u[1:2];
+            contact_constraints_inequality_1(timestep, x, u);
+            u - [-10.0; -10.0; zeros(nu - 2)];
+            [10.0; 10.0] - u[1:2];
         ]
     end
-    function inequality_t(x, u, w)
+
+    function inequality_t(x, u)
         [
-        contact_constraints_inequality_t(h, x, u, w);
-        u - [-10.0; -10.0; zeros(nu - 2)];
-        [10.0; 10.0] - u[1:2];
+            contact_constraints_inequality_t(timestep, x, u);
+            u - [-10.0; -10.0; zeros(nu - 2)];
+            [10.0; 10.0] - u[1:2];
         ]
     end
-    function inequality_T(x, u, w)
+
+    function inequality_T(x, u)
         [
-        contact_constraints_inequality_T(h, x, u, w);
+            contact_constraints_inequality_T(timestep, x, u);
         ]
     end
-    ineq1 = CALIPSO.Constraint(inequality_1, nx, nu)
-    ineqt = CALIPSO.Constraint(inequality_t, 2nx + 4, nu)
-    ineqT = CALIPSO.Constraint(inequality_T, 2nx + 4, 0)
-    ineq = [ineq1, [ineqt for t = 2:T-1]..., ineqT];
-    
-    soc = [[Constraint()] for t = 1:T] 
-    
+
+    nonnegative = [
+            inequality_1, 
+            [inequality_t for t = 2:horizon-1]..., 
+            inequality_T,
+    ];
+
+
+    # ## solver 
+    solver = Solver(objective, dynamics, num_states, num_actions; 
+        equality=equality,
+        nonnegative=nonnegative);
+
+
     # ## initialize
-    x_interpolation = linear_interpolation(x1, xT, T)
-    x_guess = [x_interpolation[1], [[x_interpolation[t]; zeros(4); x_interpolation[t]] for t = 2:T]...]
-    u_guess = [[0.0 * randn(RoboDojo.box.nu); 1.0e-1 * ones(nu - RoboDojo.box.nu)] for t = 1:T-1] # may need to run more than once to get good trajectory
-    
-    # ## problem
-    trajopt = CALIPSO.TrajectoryOptimizationProblem(dyn, obj, eq, ineq, soc);
-    methods = ProblemMethods(trajopt);
-    idx_nn, idx_soc = CALIPSO.cone_indices(trajopt)
-    
-    # ## solver
-    solver = Solver(methods, trajopt.dimensions.total_variables, trajopt.dimensions.total_parameters, trajopt.dimensions.total_equality, trajopt.dimensions.total_cone,
-        nonnegative_indices=idx_nn, 
-        second_order_indices=idx_soc,
-        options=Options(
-            verbose=true
-        ));
-    initialize_states!(solver, trajopt, x_guess);
-    initialize_controls!(solver, trajopt, u_guess);
-    
-    # solve
+    x_interpolation = linear_interpolation(state_initial, state_goal, horizon)
+    state_guess = [x_interpolation[1], [[x_interpolation[t]; zeros(4); x_interpolation[t]] for t = 2:horizon]...]
+    action_guess = [[0.0 * randn(RoboDojo.box.nu); 1.0e-1 * ones(nu - RoboDojo.box.nu)] for t = 1:horizon-1] # may need to run more than once to get good trajectory
+    initialize_states!(solver, state_guess) 
+    initialize_controls!(solver, action_guess)
+
+    # ## solve
     solve!(solver)
-    
-    x_sol, u_sol = CALIPSO.get_trajectory(solver, trajopt)
-    
-    # test solution
+
+    # ## solution
+    x_sol, u_sol = get_trajectory(solver)
+
+    # ## test
     @test norm(solver.data.residual.all, solver.options.residual_norm) / solver.dimensions.total < solver.options.residual_tolerance
 
     slack_norm = max(
@@ -272,4 +284,4 @@ end
 # ## visualize
 # vis = Visualizer()
 # render(vis)
-# RoboDojo.visualize!(vis, RoboDojo.box, x_sol, Δt=h, r=0.5);
+# RoboDojo.visualize!(vis, RoboDojo.box, x_sol, Δt=timestep, r=0.5);
